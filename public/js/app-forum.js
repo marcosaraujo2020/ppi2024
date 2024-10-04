44d753cb52c97c86e2199e75b4757661297248c6
Marcos Araujo <marcosaraujo2015.1@gmail.com>
2024-10-04 12:19:24 -0300

    atualizando front

--- public/js/app-forum.js
+++ public/js/app-forum.js
@@ -2,81 +2,155 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     var novoFormulario = document.getElementById('formulario');
-
-   
-   /*  api.sub_forum.list().then(response => {
-        var teste = document.getElementById('testando');
-        
-        console.log(response.body.rows)
-        response.body.rows.forEach(subforum => {
-            var texto = document.createElement('h3');
-            var p = document.createElement('p');
-            texto.innerText = response.body.rows[subforum.id].nome
-            p.innerText = response.body.rows[subforum.id].descricao
-            teste.appendChild(texto)
-            teste.appendChild(p)
-        })       
-    }) 
- */
-   
-
-    api.sub_forum.list().then(response => {
     
-      var section = document.getElementById("teste");
-      section.className = 'topico';
-               
-        console.log(response.body.rows)
-        response.body.rows.forEach(subforum => {
-            var ancora = document.createElement('a');
-            var div = document.createElement('div');
-            var titulo_topico = document.createElement('h4');
-            var descricao_topico = document.createElement('p');
-            var article = document.createElement('article');
-            var hr = document.createElement('hr');
-            var div_feedback = document.createElement('div');
-            var div_esquerda = document.createElement('div');
-            var div_icones = document.createElement('div');
-            var span_icone = document.createElement('span');
-            var span_icone2 = document.createElement('span');
+    var id_subforum = 1;
+   
+    if (window.location.pathname.includes('sub-forum.html')) {
+        var subforumId = getQueryParameter('id');
 
-            div_feedback.className ='feedback';
-            div_icones.className = 'icones';
-            span_icone.className = 'material-symbols-outlined';
-            span_icone.innerText = 'visibility';
-            span_icone2.className = 'material-symbols-outlined';
-            span_icone2.innerText = 'mode_comment';
-            div.className = 'conteudo-forum';
-            ancora.href = 'sub-forum.html?id=' + subforum.id;
-            ancora.className = 'link-titulo-topico';
-            titulo_topico.className = 'titulo-topico';
-            descricao_topico.className = 'descricao-topico';
-            article.className = 'post-forum';
-            
-            titulo_topico.innerText = subforum.nome;
-            descricao_topico.innerText = subforum.descricao;
-            
-            ancora.appendChild(titulo_topico);
-          
-            div.appendChild(ancora);
-            div.appendChild(descricao_topico);
-            div_icones.appendChild(span_icone);
-            div_icones.appendChild(span_icone2);
-            div_feedback.appendChild(div_esquerda);
-            div_feedback.appendChild(div_icones);
-            article.appendChild(div);
-            article.appendChild(div_feedback);
-            section.appendChild(article);
-            section.appendChild(hr);
-        })       
-    })
         
+
+        if (subforumId) {
+            api.sub_forum.get(subforumId).then(response => {
+                var subforum = response.body;
+                
+
+                console.log(subforum);
+
+                var titulo_sub_forum = document.getElementById('titulo-sub-forum');
+                var section_topico = document.getElementById('topico-sub-forum');
+                titulo_sub_forum.innerText = subforum.nome
+
+                api.topico.list(subforumId).then(response => {
+                    var topicos = response.body;
+                   
+                    topicos.rows.forEach(element => {
+                        var post_forum_topico = document.createElement('article');
+                        var ancora_topico = document.createElement('a');
+                        var descricao_topico2 = document.createElement('p');
+                        var autor_post = document.createElement('p');
+                        var data_post = document.createElement('p');
+                        var hr = document.createElement('hr');
+                        var autor_data = document.createElement('div');
+
+                        autor_data.className = "autor-data";
+                        ancora_topico.href = "mensagem.html?id=" + element.id;
+                        post_forum_topico.className = "post-forum-topico";
+                        descricao_topico2.innerText = element.titulo;
+                        autor_post.innerText = element.usuario_nome;
+                        data_post.innerText = ", " + element.created_at;
+                        ancora_topico.appendChild(descricao_topico2);
+                        autor_data.appendChild(autor_post);
+                        autor_data.appendChild(data_post);
+                        post_forum_topico.appendChild(ancora_topico);
+                        post_forum_topico.appendChild(autor_data)
+                        section_topico.appendChild(post_forum_topico);
+                        section_topico.appendChild(hr);
+
+                    });
+                })
+               
+
+            }).catch(error => {
+                console.error("Erro ao obter subfórum:", error);
+            });
+        }
+    } else {
+        api.sub_forum.list().then(response => {
+    
+            var section = document.getElementById("teste");
+            section.className = 'topico';
+                   
+            console.log(response.body.rows);
+    
+            response.body.rows.forEach(subforum => {
+                var ancora = document.createElement('a');
+                var div = document.createElement('div');
+                var titulo_topico = document.createElement('h4');
+                var descricao_topico = document.createElement('p');
+                var article = document.createElement('article');
+                var hr = document.createElement('hr');
+                var div_feedback = document.createElement('div');
+                var div_esquerda = document.createElement('div');
+                var div_icones = document.createElement('div');
+                var span_icone = document.createElement('span');
+                var span_icone2 = document.createElement('span');
+    
+                div_feedback.className ='feedback';
+                div_icones.className = 'icones';
+                span_icone.className = 'material-symbols-outlined';
+                span_icone.innerText = 'visibility';
+                span_icone2.className = 'material-symbols-outlined';
+                span_icone2.innerText = 'mode_comment';
+                div.className = 'conteudo-forum';
+                ancora.href = 'sub-forum.html?id=' + subforum.id;
+                ancora.className = 'link-titulo-topico';
+                titulo_topico.className = 'titulo-topico';
+                descricao_topico.className = 'descricao-topico';
+                article.className = 'post-forum';
+                
+                titulo_topico.innerText = subforum.nome;
+                descricao_topico.innerText = subforum.descricao;
+                
+                ancora.appendChild(titulo_topico);
+              
+                div.appendChild(ancora);
+                div.appendChild(descricao_topico);
+                div_icones.appendChild(span_icone);
+                div_icones.appendChild(span_icone2);
+                div_feedback.appendChild(div_esquerda);
+                div_feedback.appendChild(div_icones);
+                article.appendChild(div);
+                article.appendChild(div_feedback);
+                section.appendChild(article);
+                section.appendChild(hr);
+            })       
+        }).catch(error => {
+            console.error("Erro ao obter lista de subfóruns:", error);
+        });
+    }
+
+    
+
+    if (window.location.pathname.includes('mensagem.html')) {
+        
+        var mensagemId = getQueryParameter('id');       
+    
+        if (mensagemId) {
+            api.mensagem.list(id_subforum, mensagemId).then(response => {
+                var mensagem = response.body;
+                
+                mensagem.rows.forEach(element => {
+                    document.getElementById('titulo-mensagem').innerText = element.usuario_nome;
+                    document.getElementById('conteudo-mensagem').innerText = element.texto;
+                })
+    
+            }).catch(error => {
+                console.error("Erro ao obter mensagem:", error);
+                document.getElementById('titulo-mensagem').innerText = "Erro ao carregar a mensagem";
+                document.getElementById('conteudo-mensagem').innerText = "Não foi possível carregar o conteúdo da mensagem.";
+            });
+        } else {
+            document.getElementById('titulo-mensagem').innerText = "Mensagem não encontrada";
+            document.getElementById('conteudo-mensagem').innerText = "O ID da mensagem não foi fornecido.";
+        }
+    }
+    
+
+    function getQueryParameter(param) {
+        var urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(param);    
+    }
+    
+   
+    
 
     document.getElementById('novotopico').addEventListener('click', function(){
         novoFormulario.classList.remove('oculto');
     });
 
 
-    document.getElementById('novaPostagemForm').addEventListener("submit", async function (event) {
+    document.getElementById('novoTopicoForm').addEventListener("submit", async function (event) {
         event.preventDefault();
     
         const titulo = document.getElementById('titulo').value.trim();
@@ -116,6 +190,8 @@ document.addEventListener('DOMContentLoaded', function() {
         await api.auth.logoff();
         location.reload();
     });
+
+
 });
 
 
